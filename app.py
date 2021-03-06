5e2acb3f20e0f85286994568ba8d23702a3aefec
IgorLevinX <igorle1994@gmail.com>
2021-03-06 17:23:59 +0200

    Flask

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 color = os.environ.get('APP_COLOR')
+color = "green"
 
 @app.route("/")
 def main():
-    return f'[html][body style="background:{color}"][h1][b]Welcome flask user![/b][/h1][/body][/html]';
+    return f'<html><body style="background:{color}"><h1><b>Welcome flask user!</b></h1></body></html>';
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=8080)
